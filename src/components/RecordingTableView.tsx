af6cba1fb597801379233c6f7916569b852edcc9
Fynn <fynn@fynnyoung.com>
2026-07-08 23:13:21 +0200

    fix column names clipping

--- src/components/RecordingTableView.tsx
+++ src/components/RecordingTableView.tsx
@@ -269,14 +269,14 @@ export function RecordingTableView({
                     )}
                     <div
                       onMouseDown={makeResizeHandler(header.column.id)}
-                      className={`absolute right-0 top-0 h-full w-4 cursor-col-resize select-none touch-none flex items-center justify-end pr-0.5 ${
-                        isResizing
-                          ? "opacity-100"
-                          : "opacity-30 hover:opacity-100"
-                      }`}
+                      className="absolute right-0 top-0 h-full w-4 cursor-col-resize select-none touch-none flex items-center justify-end pr-0.5 bg-zinc-900 group/resize"
                     >
                       <div
-                        className={`h-3/4 w-px ${isResizing ? "bg-zinc-300" : "bg-zinc-600"}`}
+                        className={`h-3/4 w-px ${
+                          isResizing
+                            ? "bg-zinc-300"
+                            : "bg-zinc-600 opacity-30 group-hover/resize:opacity-100"
+                        }`}
                       />
                     </div>
                   </th>
